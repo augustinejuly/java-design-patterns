2d89fa788f0411eeb8e46e4399c167618015fc76
Augustine <augustinejuly@gmail.com>
2013-03-16 21:09:30 +0530

    prototype pattern added

---
+++ src/main/java/creational/org/pattern/prototype/SimpleCloning.java
@@ -0,0 +1,22 @@
+package org.pattern.prototype;
+
+/**
+ *This class creates a new object and copies all the original object's attribute values 
+ */
+public class SimpleCloning{
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public SimpleCloning(String name) {
+		this.name = name;
+	}
+
+	public SimpleCloning createCopy(){
+		return new SimpleCloning(this.name);
+	}
+
+}
