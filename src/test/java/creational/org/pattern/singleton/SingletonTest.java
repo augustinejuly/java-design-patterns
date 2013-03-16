afee5b7c00444d2412a0c67be8c0f04391d0db5c
Augustine <augustinejuly@gmail.com>
2013-03-16 21:08:28 +0530

    test case edited

--- src/test/java/creational/org/pattern/singleton/SingletonTest.java
+++ src/test/java/creational/org/pattern/singleton/SingletonTest.java
@@ -11,14 +11,14 @@ public class SingletonTest {
 	public void testEarlySingleton() throws Exception {
 		EarlySingleton e1 = EarlySingleton.getInstance();
 		EarlySingleton e2 = EarlySingleton.getInstance();
-		Assert.assertEquals(e1, e2);
+		Assert.assertTrue(e1==e2);
 	}
 	
 	@Test
 	public void testLazySingleton() throws Exception {
 		LazySingleton l1 = LazySingleton.getInstance();
 		LazySingleton l2 = LazySingleton.getInstance();
-		Assert.assertEquals(l1, l2);		
+		Assert.assertTrue(l1==l2);
 	}
 
 }
